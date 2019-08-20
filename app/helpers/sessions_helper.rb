7f671739f637b04fce39f20b67fff02adf454753
hongphuc <hongphuc.dthp@gmail.com>
2019-08-20 20:54:07 +0700

    Logging out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,12 @@ module SessionsHelper
     def log_in(user)
         session[:user_id] = user.id
     end
+
+    # Logs out the current user.
+    def log_out
+        session.delete(:user_id)
+        @current_user = nil
+    end
     
     #Return the current logged-in user(if any)
     def current_user 
