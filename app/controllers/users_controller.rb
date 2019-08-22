870eb750d397289a5057e2843c387d3b38072fc2
hongphuc <hongphuc.dthp@gmail.com>
2019-08-22 11:33:28 +0700

    Authorization : Requiring logged-in users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update] #Filters are methods that are run "before", "after" or "around" a controller action.
+
   def show 
     @user = User.find(params[:id])
     # degbugger
@@ -38,4 +40,12 @@ class UsersController < ApplicationController
     def user_params 
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end 
+
+    # Confirms a logged-in user.
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
 end
