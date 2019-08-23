e9b14c212bff81fa67642c1815dd52da49f4633d
hongphuc <hongphuc.dthp@gmail.com>
2019-08-23 10:48:23 +0700

    Account activation data model : Seed  and fixture users, refactoring downcase_email method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
     before_save   :downcase_email 
     before_create :create_activation_digest  
-    
+
     validates :name,  presence: true, length: { maximum: 50 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
@@ -51,7 +51,7 @@ class User < ApplicationRecord
     private
         #Converts email to all lower-case 
         def downcase_email
-            self.email = email.downcase
+            email.downcase!
         end
 
         #Creates and assigns the activation token and digest 
