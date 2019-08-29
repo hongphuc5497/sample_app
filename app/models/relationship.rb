603871b665a3937c90f0575277fe2fabe9f18ae5
hongphuc <hongphuc.dthp@gmail.com>
2019-08-29 14:42:01 +0700

    The Relationship model : Relationship validations

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,4 +1,7 @@
 class Relationship < ApplicationRecord
     belongs_to :followed_id, class_name: "User"
     belongs_to :follower_id, class_name: "User"
+    
+    validates  :followed_id, presence: true 
+    validates  :follower_id, presence: true 
 end
